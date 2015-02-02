e5dcc54fcc338f25a517a4a150dff6f247ef20d9
kipizik <volnesh@gmail.com>
2015-02-02 19:53:36 +0200

    new project was added

---
+++ Hotline_Autotests/Hotline_Autotests/Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hotline_Autotests.Helpers;
+using NUnit.Framework;
+using OpenQA.Selenium.Firefox;
+
+namespace Hotline_Autotests
+{
+    [TestFixture]
+    public class Tests
+    {
+        private String URL = FileOperations.GetUrl("Configuration.xml");
+
+        [SetUp]
+        public void BeforeTest()
+        {
+
+        }
+
+        [Test]
+        public void OpenMainPage()
+        {
+            var driver = new FirefoxDriver(); 
+
+            driver.Navigate().GoToUrl(URL);
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+
+        }
+
+    }
+}
